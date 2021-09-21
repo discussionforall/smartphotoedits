9148295672d5b96d8f267491b06a0ae455270bab
discussionforall <discussionforall@gmail.com>
2021-09-21 16:10:25 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -43,7 +43,7 @@ const Header = ({metaTitle, metaDescription }) => {
             <div className="col-lg-9 col-md-9 col-8">
               <div className="menu-bar">
                 <Link to="/about">About Us</Link>
-                <Link to="/sample">Sample</Link>
+                <Link to="/sample">Samples</Link>
 
                 <div className="menu-btn">
                   <button>get a quote</button>
