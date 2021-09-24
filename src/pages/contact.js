f2b1bdd43a12a7e4e0e525ac26c56c1ba235f782
discussion4all <discussion4all@yahoo.com>
2021-09-24 15:43:51 +0530

    solve design issue
    solve design issue

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -65,11 +65,11 @@ const Contact = () => {
                                     <img src={location_icon}></img>
                                 </div>    
                                 <h2>OFFICE LOCATION</h2>
-                                <a href="https://www.google.com/maps/place/Dodda+Banaswadi+Main+Rd,+Bengaluru,+Karnataka/@13.0084742,77.6400735,17z/data=!3m1!4b1!4m8!1m2!2m1!1shttps:%2F%2Fwww.google.com%2Fmaps%2Fplace%2FDodda%2BBanaswadi%2BMain%2BRd,%2BLakshmamma%2BLayout,%2BBanaswadi,%2BBengaluru,%2BKarnataka%2F@13.014965,77.6532301,17z%2Fdata%3D*213m1*214b1*214m5*213m4*211s0x3bae16e81befbe45:0x4ebce86830dab029*218m2*213d13.0149598*214d77.6554241!3m4!1s0x3bae16e81befbe45:0x4ebce86830dab029!8m2!3d13.008469!4d77.6422675" target="_blank">
+                                <p>
                                     Dodda Banaswadi Main Rd, Lakshmamma Layout, Dodda Banaswadi, Bengaluru, Karnataka 560043
-                                </a>
+                                </p>
                             </div>        
-                            <a href="#">See on map</a>   
+                            <a href="https://www.google.com/maps/place/Dodda+Banaswadi+Main+Rd,+Bengaluru,+Karnataka/@13.0084742,77.6400735,17z/data=!3m1!4b1!4m8!1m2!2m1!1shttps:%2F%2Fwww.google.com%2Fmaps%2Fplace%2FDodda%2BBanaswadi%2BMain%2BRd,%2BLakshmamma%2BLayout,%2BBanaswadi,%2BBengaluru,%2BKarnataka%2F@13.014965,77.6532301,17z%2Fdata%3D*213m1*214b1*214m5*213m4*211s0x3bae16e81befbe45:0x4ebce86830dab029*218m2*213d13.0149598*214d77.6554241!3m4!1s0x3bae16e81befbe45:0x4ebce86830dab029!8m2!3d13.008469!4d77.6422675" target="_blank">See on map</a>   
                         </div>
                     </div>
              
@@ -84,7 +84,7 @@ const Contact = () => {
                                 <p>Call us 24/7</p>
                                 <h3>+00 80 555 777</h3>
                             </div>    
-                            <a href="#">Call Us Now</a> 
+                            <a href="tel:+00 80 555 777">Call Us Now</a> 
                         </div>
                     </div>
               
@@ -97,9 +97,9 @@ const Contact = () => {
                                  </div>   
                                 <h2>OFFICE EMAIL</h2>
                                 <p>Call us 24/7</p>
-                                <h3>info@smartphotoedits.com</h3>   
+                                <h3>info@smartphotoedits.cominfo@smartphotoedits.com</h3>   
                             </div>    
-                            <a href="#">Email Us Now</a> 
+                            <a href="mailto:info@smartphotoedits.com">Email Us Now</a> 
                         </div>
                     </div>
                 </div>
