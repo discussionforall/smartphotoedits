d050d5ae00b9504095622a6e62b97f9193595d6b
discussion4all <discussion4all@yahoo.com>
2021-10-01 10:18:50 +0530

    Add more button design
    Add more button design

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -75,51 +75,8 @@ return (
                         
                      </div>
                     </div>
-                  </TabPanel>
-                  <TabPanel>
-                  <div className="after-slider">
-                   <ReactBeforeSliderComponent
-                    
-                      firstImage={FIRST_IMAGE}
-                      secondImage={SECOND_IMAGE}
-                    />
-                     <div className="after-slider-text">
-                        <div className="left-after-text">
-                            <h1>Real Estate House <br></br> Photoshooting Image Edit</h1>
-                            <h1>2021</h1>
-                        </div>
-                        
-                     </div>
-                    </div>
-                    <div className="after-slider">
-                   <ReactBeforeSliderComponent
-                    
-                      firstImage={FIRST_IMAGE}
-                      secondImage={SECOND_IMAGE}
-                    />
-                     <div className="after-slider-text">
-                        <div className="left-after-text">
-                            <h1>Real Estate House <br></br> Photoshooting Image Edit</h1>
-                            <h1>2021</h1>
-                        </div>
-                        
-                     </div>
-                    </div>
-                  </TabPanel>
-                  <TabPanel>
-                  <div className="after-slider">
-                   <ReactBeforeSliderComponent
-                    
-                      firstImage={FIRST_IMAGE}
-                      secondImage={SECOND_IMAGE}
-                    />
-                     <div className="after-slider-text">
-                        <div className="left-after-text">
-                            <h1>Real Estate House <br></br> Photoshooting Image Edit</h1>
-                            <h1>2021</h1>
-                        </div>
-                        
-                     </div>
+                    <div className="load-more-button">
+                       <button>load More <span><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-down" class="svg-inline--fa fa-long-arrow-alt-down fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z"></path></svg></span></button>
                     </div>
                   </TabPanel>
                   <TabPanel>
@@ -151,6 +108,10 @@ return (
                         
                      </div>
                     </div>
+                    <div className="load-more-button">
+                       <button>load More <span><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-down" class="svg-inline--fa fa-long-arrow-alt-down fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z"></path></svg></span></button>
+                    </div>
+                    
                   </TabPanel>
                   <TabPanel>
                   <div className="after-slider">
@@ -166,6 +127,62 @@ return (
                         </div>
                         
                      </div>
+                    </div>
+                    <div className="load-more-button">
+                       <button>load More <span><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-down" class="svg-inline--fa fa-long-arrow-alt-down fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z"></path></svg></span></button>
+                    </div>
+                  </TabPanel>
+                  <TabPanel>
+                  <div className="after-slider">
+                   <ReactBeforeSliderComponent
+                    
+                      firstImage={FIRST_IMAGE}
+                      secondImage={SECOND_IMAGE}
+                    />
+                     <div className="after-slider-text">
+                        <div className="left-after-text">
+                            <h1>Real Estate House <br></br> Photoshooting Image Edit</h1>
+                            <h1>2021</h1>
+                        </div>
+                        
+                     </div>
+                    </div>
+                    <div className="after-slider">
+                   <ReactBeforeSliderComponent
+                    
+                      firstImage={FIRST_IMAGE}
+                      secondImage={SECOND_IMAGE}
+                    />
+                     <div className="after-slider-text">
+                        <div className="left-after-text">
+                            <h1>Real Estate House <br></br> Photoshooting Image Edit</h1>
+                            <h1>2021</h1>
+                        </div>
+                        
+                     </div>
+                    </div>
+                    <div className="load-more-button">
+                       <button>load More <span><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-down" class="svg-inline--fa fa-long-arrow-alt-down fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z"></path></svg></span></button>
+                    </div>
+                  </TabPanel>
+               
+                  <TabPanel>
+                  <div className="after-slider">
+                   <ReactBeforeSliderComponent
+                    
+                      firstImage={FIRST_IMAGE}
+                      secondImage={SECOND_IMAGE}
+                    />
+                     <div className="after-slider-text">
+                        <div className="left-after-text">
+                            <h1>Real Estate House <br></br> Photoshooting Image Edit</h1>
+                            <h1>2021</h1>
+                        </div>
+                        
+                     </div>
+                    </div>
+                    <div className="load-more-button">
+                       <button>load More <span><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="long-arrow-alt-down" class="svg-inline--fa fa-long-arrow-alt-down fa-w-8" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M168 345.941V44c0-6.627-5.373-12-12-12h-56c-6.627 0-12 5.373-12 12v301.941H41.941c-21.382 0-32.09 25.851-16.971 40.971l86.059 86.059c9.373 9.373 24.569 9.373 33.941 0l86.059-86.059c15.119-15.119 4.411-40.971-16.971-40.971H168z"></path></svg></span></button>
                     </div>
                   </TabPanel>
                   </div>
