1324b2e00ae5bf6651908912527f7a1d9ba1b90b
Milan <milan.zalavadiya@yahoo.com>
2021-10-13 17:13:39 +0530

    set address and map section

--- src/pages/thankYou.js
+++ src/pages/thankYou.js
@@ -7,6 +7,9 @@ import email_icon from "../images/email-icon.webp"
 import { useForm } from "react-hook-form"
 import axios from "axios"
 import { commonConfig } from "../commonConfig/config"
+import GoogleMapCode from "../components/googleMapCode"
+import AddressSection from "../components/addressSection"
+
 const ThankYou = () => {
   return (
     <>
@@ -35,73 +38,11 @@ const ThankYou = () => {
             will contact you in 1 business day.
           </div>
 
-          <div className="address-sec">
-            <div className="address-row">
-              <div className="address-text">
-                <div className="address-bg">
-                  <div className="text-add">
-                    <div className="img-add">
-                      <img src={location_icon}></img>
-                    </div>
-                    <h2>OFFICE LOCATION</h2>
-                    <p>
-                      Dodda Banaswadi Main Rd, Lakshmamma Layout, Dodda
-                      Banaswadi, Bengaluru, Karnataka 560043
-                    </p>
-                  </div>
-                  <a
-                    href="https://www.google.com/maps/place/Smart+Photo+Edits/@13.0136118,77.659181,17z/data=!3m1!4b1!4m5!3m4!1s0x3bae114392e8f37f:0x8f482cafd65ddd13!8m2!3d13.0136066!4d77.661375"
-                    target="_blank"
-                  >
-                    See on map
-                  </a>
-                </div>
-              </div>
-
-              <div className="address-text">
-                <div className="address-bg">
-                  <div className="text-add">
-                    <div className="img-add">
-                      <img src={phone_icon}></img>
-                    </div>
-                    <h2>OFFICE PHONE</h2>
-                    <p>Call us 24/7</p>
-                    <h3>+00 80 555 777</h3>
-                  </div>
-                  <a href="tel:+00 80 555 777">Call Us Now</a>
-                </div>
-              </div>
-
-              <div className="address-text">
-                <div className="address-bg">
-                  <div className="text-add">
-                    <div className="img-add">
-                      <img src={email_icon}></img>
-                    </div>
-                    <h2>OFFICE EMAIL</h2>
-                    <p>Call us 24/7</p>
-                    <h3>info@smartphotoedits.com</h3>
-                  </div>
-                  <a href="mailto:info@smartphotoedits.com">Email Us Now</a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <AddressSection/>
         </div>
 
-        <div className="google-map-code">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.3462842377826!2d77.65918631482246!3d13.013606590828319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae114392e8f37f%3A0x8f482cafd65ddd13!2sSmart%20Photo%20Edits!5e0!3m2!1sen!2sin!4v1634023023078!5m2!1sen!2sin"
-            width="100%"
-            height="600"
-            frameborder="0"
-            style={{ border: 0 }}
-            allowfullscreen=""
-            aria-hidden="false"
-            tabindex="0"
-          ></iframe>
-        </div>
-
+        <GoogleMapCode/>
+        
         <div class="start-sec privacy-6">
           <div class="container">
             <div class="row  align-items-center">
